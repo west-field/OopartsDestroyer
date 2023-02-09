9f610a159e21abaa0df30bd2f51d32d1c18d6066
west-field <fko2247034@stu.o-hara.ac.jp>
2023-02-09 11:53:03 +0900

    でバック機能を実装、画面サイズを指定（仮）

--- game.h
+++ game.h
@@ -11,17 +11,26 @@ namespace Game
 	//ウィンドウ名
 	const wchar_t* const kTitleText = L"ロックマン";
 	//ウィンドウサイズ
-	constexpr int kScreenWidth = 1600;//16
+	constexpr int kScreenWidth = 1600;//16:
 	constexpr int kScreenHeight = 900;//9
 	//カラーモード
 	constexpr int kColorDepth = 32;	//32 or 16
 	//マップチップサイズ
 	constexpr int ChipSize = 32;
+	constexpr float kScale = 2.0f;
 
 	//画面におけるマップチップの数
 	static constexpr int kNumX = kScreenWidth / ChipSize;
 	static constexpr int kNumY = kScreenHeight / ChipSize;
+
+	//画面に表示できるマップの大きさ
+	static constexpr int kMapNumX = 16;
+	static constexpr int kMapNumY = 16;
+	//マップを表示する画面サイズ
+	static constexpr int kMapScreenX = (kScreenWidth - kMapNumX * ChipSize)/2;
+	static constexpr int kMapScreenY = (kScreenHeight - kMapNumY * ChipSize)/2;
+
 	//マップの大きさ
-	static constexpr int kMapChipNumX = kNumX + 46;// 219;// Game::kScreenWidth / Game::ChipSize * 2;
-	static constexpr int kMapChipNumY = kNumY + 47;//135;//Game::kScreenHeight / Game::ChipSize;
+	static constexpr int kMapChipNumX = 96;//219;
+	static constexpr int kMapChipNumY = 92;//135;
 };
