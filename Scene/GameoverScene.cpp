f74e72b8d04ae0299a770dc5fa610b09cf9e4042
west-field <fko2247034@stu.o-hara.ac.jp>
2023-03-14 14:11:58 +0900

    Restart(GameplayingSceneに移動)できるようにした

--- Scene/GameoverScene.cpp
+++ Scene/GameoverScene.cpp
@@ -7,6 +7,7 @@
 #include "../Util/DrawFunctions.h"
 #include "SceneManager.h"
 #include "TitleScene.h"
+#include "GameplayingScene.h"
 
 #include "../Game/Player.h"
 
@@ -25,6 +26,7 @@ namespace
 
 	constexpr float kMojiNum = 30.0f;
 	constexpr float kMoveNum = 2.0f;
+	constexpr int kMenuFontSize = 50;
 }
 GameoverScene::GameoverScene(SceneManager& manager, std::shared_ptr<Player>player) :
 	Scene(manager), m_player(player), m_updateFunc(&GameoverScene::FadeInUpdat),
@@ -34,7 +36,7 @@ GameoverScene::GameoverScene(SceneManager& manager, std::shared_ptr<Player>playe
 	float posX = (Game::kScreenWidth - kMojiNum * kMojiSize) / 2;
 	for (int i = 0; i < kMojiNum; i++)
 	{
-		m_moji[i].pos = { static_cast<float>(posX + i * kMojiSize) ,Game::kScreenHeight / 2};
+		m_moji[i].pos = { static_cast<float>(posX + i * kMojiSize) ,Game::kScreenHeight / 3};
 		m_moji[i].moveY = i * -1.0f;
 		m_moji[i].add = 0.5f;
 	}
@@ -72,6 +74,7 @@ void GameoverScene::FadeInUpdat(const InputState& input)
 	if (--m_fadeTimer == 0)
 	{
 		m_updateFunc = &GameoverScene::NormalUpdat;
+		m_fadeColor = 0x000000;
 		m_fadeValue = 0;
 		SoundManager::GetInstance().Play(SoundId::Gameover);
 	}
@@ -131,10 +134,43 @@ void GameoverScene::MojiUpdate(const InputState& input)
 		moji.moveY += moji.add;
 	}
 
+	//メニュー
+	bool isPress = false;//キーが押されたかどうか
+	if (input.IsTriggered(InputType::down))
+	{
+		m_selectNum = (m_selectNum + 1) % menuNum;//選択状態を一つ下げる
+		isPress = true;
+	}
+	else if (input.IsTriggered(InputType::up))
+	{
+		m_selectNum = (m_selectNum + (menuNum - 1)) % menuNum;//選択状態を一つ上げる
+		isPress = true;
+	}
+
+	if (isPress)
+	{
+		SoundManager::GetInstance().Play(SoundId::Cursor);
+		for (int i = 0; i < menuNum; i++)
+		{
+			if (i == m_selectNum)
+			{
+				SelectMenu[i].x = kMovedPosX;//移動位置に移動する
+				SelectMenu[i].color = 0xffa0aa;//色を変える
+			}
+			else
+			{
+				SelectMenu[i].x = kOriginalPosX;//元の位置に戻す
+				SelectMenu[i].color = 0xaaa0ff;//元の色に戻す
+			}
+		}
+	}
+	
+	//「次へ」ボタンが押されたら次シーンへ移行する
 	if (!m_player->IsExist() && input.IsTriggered(InputType::next))
 	{
+		SoundManager::GetInstance().Play(SoundId::Determinant);
+
 		m_updateFunc = &GameoverScene::FadeOutUpdat;
-		m_fadeColor = 0x000000;
 	}
 }
 
@@ -153,6 +189,13 @@ void GameoverScene::MojiDraw()
 		DrawStringF(m_moji[i].pos.x , m_moji[i].pos.y + m_moji[i].moveY, kMoji[i], 0xff0000);
 	}
 	SetFontSize(0);
+
+	SetFontSize(kMenuFontSize);
+	DrawString(SelectMenu[menuRestart].x + 5, SelectMenu[menuRestart].y + 5, L"最初から", 0x000000);
+	DrawString(SelectMenu[menuRestart].x, SelectMenu[menuRestart].y, L"最初から", SelectMenu[menuRestart].color);
+	DrawString(SelectMenu[menuGameEnd].x + 5, SelectMenu[menuGameEnd].y + 5, L"タイトルに戻る", 0x000000);
+	DrawString(SelectMenu[menuGameEnd].x, SelectMenu[menuGameEnd].y, L"タイトルに戻る", SelectMenu[menuGameEnd].color);
+	SetFontSize(0);
 }
 
 void GameoverScene::FadeOutUpdat(const InputState& input)
@@ -161,8 +204,16 @@ void GameoverScene::FadeOutUpdat(const InputState& input)
 	ChangeVolumeSoundMem(200 - m_fadeValue, m_BgmH);
 	if (++m_fadeTimer == kFadeInterval)
 	{
-		m_manager.ChangeScene(new TitleScene(m_manager));
-		return;
+		switch (m_selectNum)
+		{
+		case menuRestart:
+			m_manager.ChangeScene(new GameplayingScene(m_manager));
+			return;
+		case menuGameEnd:
+		default:
+			m_manager.ChangeScene(new TitleScene(m_manager));
+			return;
+		}
 	}
 }
 
--- Scene/GameoverScene.h
+++ Scene/GameoverScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Secne.h"
 #include "../Util/Geometry.h"
+#include "../game.h"
 
 #include <memory>//スマートポインタをつかうため
 #include <array>
@@ -37,6 +38,7 @@ private:
     //プレイヤー
     std::shared_ptr<Player> m_player;
 
+    //文字
     struct Moji
     {
         Position2 pos;
@@ -47,5 +49,30 @@ private:
     static constexpr int kMojiSize = 80;
     std::array<Moji, kMojiNum> m_moji;
     int  m_soundVolume = 0;
+
+    static constexpr int kMenuFontSize = 50;//文字のサイズ
+    static constexpr int kOriginalPosX = Game::kScreenWidth / 3 + kMenuFontSize * 2;    //メニュー文字のx位置
+    static constexpr int kOriginalPosY = Game::kScreenHeight / 2 + kMenuFontSize * 2;    //メニュー文字のy位置
+    static constexpr int kMovedPosX = kOriginalPosX - kMenuFontSize;//メニュー文字の移動したx位置
+    //メニュー項目
+    enum MenuItem
+    {
+        menuRestart,	//リスタート
+        menuGameEnd,	//ゲーム終了
+
+        menuNum			//項目の数
+    };
+    //メニュー要素
+    struct MenuElement
+    {
+        int x;
+        int y;				//座標
+        unsigned int color;//色
+    };
+    MenuElement SelectMenu[menuNum] = {
+        { kMovedPosX, kOriginalPosY + kMenuFontSize * menuRestart,0xffa0aa  },
+        { kOriginalPosX, kOriginalPosY + kMenuFontSize * menuGameEnd + 5,0xaaa0ff}
+    };
+    int m_selectNum = 0;//選択しているメニュー項目
 };
 
